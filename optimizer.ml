3cc402f04b6738b5a73575619fa618485067bf88
jeffkf123 <148153706+jeffkf123@users.noreply.github.com>
2023-12-08 17:34:28 +0900

    new

---
+++ optimizer.ml
@@ -0,0 +1,2 @@
+let optimize : T.program -> T.program
+= fun t -> raise (Failure "Not implemented") (* TODO *)
---
+++ translator.ml
@@ -0,0 +1,2 @@
+let translate : S.program -> T.program
+= fun s -> raise (Failure "Not implemented") (* TODO *)
